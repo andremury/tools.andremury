0b2a7489a1a3f1cb1c2d564a8d6cbe73a0a1980a
andremury <jamesandre79@hotmail.com>
2025-04-11 07:44:10 -0300

    fix: updated vb, quill editor error

--- components/SRS/Editor/Content.vue
+++ components/SRS/Editor/Content.vue
@@ -16,8 +16,8 @@
                         <input :name="key" v-model="value.title" :placeholder="startCase(key)" class="srs-input w-75" />
                     </h4>
                 </label>
-                <div class="editor srs-input">
-                    <QuillEditor v-model:content="value.content" theme="bubble" :id="`quill-${key}`" content-type="html"
+                <div class="editor srs-input" v-if="QEditor">
+                    <QEditor v-model:content="value.content" theme="bubble" :id="`quill-${key}`" content-type="html"
                         :toolbar="toolbarOptions" />
                 </div>
                 <div class="requirement-specifications" v-if="model?.requirements">
@@ -43,6 +43,8 @@ import type { QuillEditor } from '@vueup/vue-quill';
 
 const mounted = ref(false);
 const model = defineModel<SRS.Specification>();
+
+const QEditor = ref<typeof QuillEditor>();
 
 const visibleSections = computed(() => Object.entries(model?.value?.sections || {}).filter(
     ([_, value]) => !value.invisible
@@ -72,9 +74,10 @@ const toolbarOptions = [
     // ['clean']                                         // remove formatting button
 ];
 
-onMounted(() => {
+onMounted(async () => {
     if (typeof document !== "undefined") {
         mounted.value = true;
+        QEditor.value = (await import('@vueup/vue-quill')).QuillEditor;
     }
 });
 </script>
