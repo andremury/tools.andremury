bf4fe6cd0e9d106dd22a894441327a33a07e0d62
andremury <jamesandre79@hotmail.com>
2025-05-14 00:17:43 -0300

    feat: checkmark on canvas item

--- components/SRS/Canvas/Item.vue
+++ components/SRS/Canvas/Item.vue
@@ -1,15 +1,30 @@
 <template>
-    <div class="srs-rf canvas-item rounded border shadow-sm p-3 d-flex flex-column align-items-center justify-content-center pointer"
-        :class="[textColor]" :style="{
-            backgroundColor: requirement.color || 'blanchedalmond'
-        }">
-        <div class="rf-content text-center">
-            <p><small>[{{ requirement.id }}]</small><br /><b> {{ requirement.title }}</b></p>
-            <small v-if="requirement.dependencies.length">
-                <{{requirement.dependencies.filter(d => d).map(d => d?.id).join(', ')}}>
-            </small>
-        </div>
+  <div
+    class="srs-rf canvas-item rounded border shadow-sm p-3 d-flex flex-column align-items-center justify-content-center pointer position-relative"
+    :class="[textColor]"
+    :style="{
+      backgroundColor: requirement.color || 'blanchedalmond',
+    }"
+  >
+    <div class="icon-completed position-absolute text-success" v-if="requirement.done">
+      <FaIcon icon="check-circle" />
     </div>
+    <div class="rf-content text-center">
+      <p>
+        <small>[{{ requirement.id }}]</small><br /><b>
+          {{ requirement.title }}</b
+        >
+      </p>
+      <small v-if="requirement.dependencies.length">
+        <{{
+          requirement.dependencies
+            .filter((d) => d)
+            .map((d) => d?.id)
+            .join(', ')
+        }}>
+      </small>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,10 +32,19 @@ import { Colors } from '~/shared/colors';
 import type { SRS } from '~/shared/types';
 
 const { requirement } = defineProps<{
-    requirement: SRS.Requirement;
+  requirement: SRS.Requirement;
 }>();
 
-const textColor = computed(() => Colors.isDarkColor(
-    Colors.hexToRgb(requirement.color || '#ffffff')
-) ? 'text-white' : 'text-dark');
+const textColor = computed(() =>
+  Colors.isDarkColor(Colors.hexToRgb(requirement.color || '#ffffff'))
+    ? 'text-white'
+    : 'text-dark'
+);
 </script>
+
+<style lang="css" scoped>
+.icon-completed {
+    right: 1px;
+    top: -4px;
+}
+</style>
